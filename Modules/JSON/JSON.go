48c1b7786dd5a35599cf0b1e061c8b0cc8ab3c19
deniztohumcu <deniz.tohumcu93@gmail.com>
2020-04-24 22:16:45 +0200

    CounrtyCalculatedValues and WorldCalculatedValues structs

--- Modules/JSON/JSON.go
+++ Modules/JSON/JSON.go
@@ -27,12 +27,31 @@ type CountryCases struct {
 	deaths      int    `json:"deaths"`      // Number of deaths in a country per day
 	recovered   int    `json:"recovered"`   // Number of recovered in a country per
 
-	// Data calculated from the grabbed data
-	closed             int //deaths + recovered // Number of closed cases in a country
-	active             int //confirmed - closed // Number of active cases in a country
-	deathPercentage    int //deaths/closed      // Percentage of death in a country
-	recoveryPercentage int //recovered/closed   // Percentage of recovery in a country
-	activePercentage   int //active/confirmed   // Percentage of active cases in a country
-	closedPercentage   int //closed/confirmed   // Percentage of closed cases in a country
+}
 
+type CountryCalculatedValues struct {
+
+	// Data calculated from the grabbed data with CountryCases object
+	closed             int // deaths + recovered // Number of closed cases in a country
+	active             int // confirmed - closed // Number of active cases in a country
+	deathPercentage    int // deaths/closed      // Percentage of death in a country
+	recoveryPercentage int // recovered/closed   // Percentage of recovery in a country
+	activePercentage   int // active/confirmed   // Percentage of active cases in a country
+	closedPercentage   int // closed/confirmed   // Percentage of closed cases in a country
+
+}
+
+type WorldCalculatedValues struct {
+
+	// Data calculated from the grabbed data with WorldCases object
+	totalRecoveredPercentage  int // totalRecovered/totalCases                // Percentage of recovery in the world
+	totalUnresolvedPercentage int // totalUnresolved/totalCases               // Percentage of death in the world
+	totalClosed               int // totalCases - activeCases                 // Number of closed cases
+	totalDeathsPercentage     int // totalDeaths/totalClosed                  // Percentage of deaths over closed
+	// cases
+	totalDeficit                int // totalNewCasesToday - totalNewDeathsToday // Value that show the change in trends
+	totalSeriousCasesPercentage int // totalSeriousCases/totalActiveCases       // Percentage of Serious cases in the
+	// world
+	totalActiveCasesPercentage int // totalActiveCases/totalCases              // Percentage of active cases in the
+	// world
 }
