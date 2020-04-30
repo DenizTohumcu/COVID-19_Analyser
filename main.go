b9126cd9ed1c00683e9d7607a0b231baa66b9bb3
deniztohumcu <deniz.tohumcu93@gmail.com>
2020-04-30 21:41:46 +0200

    Empty main Function

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"COVID-19_Analyser/Modules/API"
-	"encoding/json"
-	"fmt"
-)
-
 func main() {
-	API.HelloFunc()
-	token, response := API.Availability("https://api.thevirustracker.com/free-api?countryTotal=TR")
-	fmt.Println(token)
-	//fmt.Println(API.ReadBody(response))
-	CountryType := API.ReadBody(response)
-	//TotalCases := json.Unmarshal(CountryType,JSON.)
-	//fmt.Println(TotalCases)
+
 }
